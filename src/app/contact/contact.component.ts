baacfdb4f1a20c32775a38c4fbbb5e6e6adfcf64
Tarek Samoud <ts@sasse-mertins.de>
2019-11-15 22:50:36 +0100

    update Contact TS file

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Contact} from '../models/contact.model';
+import {ContactService} from '../services/contact.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  public recommandation:Contact;
+  constructor(
+    private contactService:ContactService,
+    private routes:Router
+    ) { }
 
   ngOnInit() {
   }
 
+  onSendClick(data:Contact) {
+    this.contactService.addRessource(this.contactService.host+"/contacts",data)
+      .subscribe(resp=>{
+        window.alert("Recommandation bien envoyée :)");
+        this.recommandation=resp;
+        this.routes.navigateByUrl("/produits");
+      },erreur=>{
+        console.log(erreur);
+      });
+  }
 }
